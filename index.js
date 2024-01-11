b9859df2046de7d594aca5034d485824747b11eb
Andrien Montmayeur <92160718+AndrienMont@users.noreply.github.com>
2024-01-11 11:22:21 +0100

    simple soap call

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var soap = require('soap');
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -7,4 +8,21 @@ app.get('/', function(req, res){
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
+});
+
+
+const url = 'http://127.0.0.1:8000/?wsdl';
+soap.createClient(url, function (err, client) {
+    if (err) {
+        console.error(err);
+    } else {
+        // Call the 'tripTime' method on the SOAP service
+        client.tripTime({ distance: 100, vitesse: 50, points: 2 }, function (err, result) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(result);
+            }
+        });
+    }
 });
