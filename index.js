f00a703e31a7ef4c3f2d127f1fb645bd322a9e84
Andrien Montmayeur <92160718+AndrienMont@users.noreply.github.com>
2024-01-25 10:40:12 +0100

    rest and graphql calls

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var soap = require('soap');
+var https = require('https');
+var axios = require('axios');
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -10,19 +12,186 @@ http.listen(3000, function(){
     console.log('listening on *:3000');
 });
 
+var tripTime = 0;
 
-const url = 'http://127.0.0.1:8000/?wsdl';
-soap.createClient(url, function (err, client) {
-    if (err) {
-        console.error(err);
-    } else {
-        // Call the 'tripTime' method on the SOAP service
-        client.tripTime({ distance: 100, vitesse: 50, points: 2 }, function (err, result) {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log(result);
-            }
+const url = 'http://127.0.0.1:8080/?wsdl';
+// soap.createClient(url, function (err, client) {
+//     if (err) {
+//         console.error(err);
+//     } else {
+//         // Call the 'tripTime' method on the SOAP service
+//         client.tripTime({ distance: 100, vitesse: 50, points: 2 }, function (err, result) {
+//             if (err) {
+//                 console.error(err);
+//             } else {
+//                 tripTime = result.tripTimeResult;
+//             }
+//         });
+//     }
+// });
+
+app.get('/tripTime', function(req, res){
+    res.status(200).send(tripTime.toString());
+});
+
+app.get('/tripTime/:distance/:vitesse/:points', function(req, res){
+    const dist = req.params.distance;
+    const vit = req.params.vitesse;
+    const poi = req.params.points;
+
+    soap.createClient(url, function(err,client){
+        if(err){
+            console.error(err);
+            res.status(500).send(err);
+        }else{
+            client.tripTime({distance: dist, vitesse: vit, points: poi}, function(err, result){
+                if(err){
+                    console.error(err);
+                    res.status(500).send(err);
+                }else{
+                    tripTime = result.tripTimeResult;
+                    console.log(tripTime);
+                    res.status(200).send(tripTime.toString());
+                }
+            });
+        }
+    })
+
+});
+
+//use GeocodeAutocompleteService for coordinates
+app.get('/coordinates/:address', function(req, res){
+    const address = req.params.address;
+    
+    const options = {
+        method: "GET",
+        hostname: "api.openrouteservice.org",
+        path: "/geocode/autocomplete?api_key=5b3ce3597851110001cf6248d3e9578fa3f8437bb98495a777283f1d&text="+address,
+        headers: {
+            "Accept": "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8"
+        }
+    };
+
+    const request = https.request(options, function(apiRes){
+        let body = '';
+
+        apiRes.on('data', (chunk) => {
+            body += chunk;
         });
+
+        apiRes.on('end', () => {
+            console.log('Status:', res.statusCode);
+            console.log('Headers:', JSON.stringify(res.headers));
+            console.log('Response:', body);
+            res.status(200).send(body);
+        });
+
+    });
+
+    request.on('error', (e) => {
+        console.error(`problem with request: ${e.message}`);
+    });
+
+    request.end();
+});
+
+app.get('/route/:startLat/:startLong/:endLat/:endLong', function(req, res){
+    const startLat = req.params.startLat;
+    const startLong = req.params.startLong;
+    const endLat = req.params.endLat;
+    const endLong = req.params.endLong;
+
+    const start = startLong+","+startLat;
+    const end = endLong+","+endLat;
+
+    const options = {
+        method: "GET",
+        hostname: "api.openrouteservice.org",
+        path: "/v2/directions/driving-car?api_key=5b3ce3597851110001cf6248d3e9578fa3f8437bb98495a777283f1d&start="+start+"&end="+end,
+        headers: {
+            "Accept": "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8"
+        }
+    };
+
+    const request = https.request(options, function(apiRes){
+        let body = '';
+
+        apiRes.on('data', (chunk) => {
+            body += chunk;
+        });
+
+        apiRes.on('end', () => {
+            console.log('Status:', res.statusCode);
+            console.log('Headers:', JSON.stringify(res.headers));
+            console.log('Response:', body);
+            res.status(200).send(body);
+        });
+
+    });
+
+    request.on('error', (e) => {
+        console.error(`problem with request: ${e.message}`);
+    });
+
+    request.end();
+});
+
+
+app.get('/vehicles', async (req, res) => {
+    const urlVehi = 'https://api.chargetrip.io/graphql';
+
+    const graphQuery = `
+    query {
+        vehicleList (size : 15){
+            naming {
+                make
+                model
+                version
+            },
+            media {
+                image {
+                    url
+                }
+            },
+            battery {
+                usable_kwh
+                full_kwh
+            },
+            adapters {
+                standard
+                power
+                max_electric_power
+                time
+                speed
+            },
+            body {
+                seats
+            },
+            range {
+                chargetrip_range {
+                    best
+                    worst
+                }
+            },
+            routing {
+                fast_charging_support
+            }
+        }
+    }
+    `;
+
+    const headers = {
+        'x-client-id' : '65aa740d0117350bae37ac78',
+        'x-app-id' : '65aa740d0117350bae37ac7a'
+    };
+
+    try {
+        const response = await axios.post(urlVehi, {query : graphQuery}, {headers});
+
+        const data = response.data.data;
+        res.json(data);
+    }catch(err){
+        console.error(err);
+        res.status(500).send("Error while fecthing the vehicles list.")
     }
 });
